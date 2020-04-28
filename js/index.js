7a22d4f4a70ae161cca8c6e62642b2df8fa42b2f
Kevin Ramos Gonzales <55323915+KevinERamos@users.noreply.github.com>
2020-04-28 18:05:47 -0500

    json update3

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 const getHelados = () => {
-    const URL = "productos.json";
+    const URL = "../productos.json";
     return fetch(URL).
         then(response => response.json())
         .then(data => data)
